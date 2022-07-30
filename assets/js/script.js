59a492a8a7e94be291af2eba78896de25cc68816
dansantinojones <dansantinojones@hotmail.co.uk>
2022-07-30 09:38:30 +0000

    Add if statements to generate gk choice randomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let gkChoiceDisplay = document.getElementById('gk-choice')
 let resultDisplay =  document.getElementById('result')
 let possibleChoice = document.querySelectorAll('button')
 let yourChoice
+let gkChoice
 
 possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     yourChoice = e.target.id
@@ -12,4 +13,15 @@ possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click'
 
 function generateGkChoice() {
     let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+    if (randomNumber === 1) {
+        gkChoice = 'Left'
+    }
+    if (randomNumber === 2) {
+        gkChoice = 'Middle'
+    }
+    if (randomNumber === 3) {
+        gkChoice = 'Right'
+    }
+    gkChoiceDisplay.innerHTML = gkChoice
 }
