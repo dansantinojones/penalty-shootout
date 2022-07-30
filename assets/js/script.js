c2bd57a8527b623916fee0f04cad676653aa8945
dansantinojones <dansantinojones@hotmail.co.uk>
2022-07-30 09:58:46 +0000

    Format document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let yourChoiceDisplay = document.getElementById('your-choice')
 let gkChoiceDisplay = document.getElementById('gk-choice')
-let resultDisplay =  document.getElementById('result')
+let resultDisplay = document.getElementById('result')
 let possibleChoice = document.querySelectorAll('button')
 let yourChoice
 let gkChoice
@@ -28,11 +28,11 @@ function generateGkChoice() {
     gkChoiceDisplay.innerHTML = gkChoice
 }
 
-function getResult () {
+function getResult() {
     if (yourChoice === gkChoice)
         result = "WHAT A SAVE!!!"
 
-    if (yourChoice !== gkChoice){
+    if (yourChoice !== gkChoice) {
         result = "GOOOAL!!!"
     }
     resultDisplay.innerHTML = result
