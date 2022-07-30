573cc11d7879140fc1e60628ae490d2b26f303da
dansantinojones <dansantinojones@hotmail.co.uk>
2022-07-30 09:22:05 +0000

    Add function to generate your choice outcome

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ let yourChoiceDisplay = document.getElementById('your-choice')
 let gkChoiceDisplay = document.getElementById('gk-choice')
 let resultDisplay =  document.getElementById('result')
 let possibleChoice = document.querySelectorAll('button')
+let yourChoice
+
+possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    yourChoice = e.target.id
+    yourChoiceDisplay.innerHTML = yourChoice
+    generateGkChoice()
+}))
+
+function generateGkChoice() {
+    let randomNumber = Math.floor(Math.random() * 3 + 1)
+}
