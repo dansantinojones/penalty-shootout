ec605da7374c21e0f3b0b8b97df54862e7156a35
dansantinojones <dansantinojones@hotmail.co.uk>
2022-07-30 09:20:47 +0000

    Add initial js variables for result section

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let yourChoiceDisplay = document.getElementById('your-choice')
+let gkChoiceDisplay = document.getElementById('gk-choice')
+let resultDisplay =  document.getElementById('result')
+let possibleChoice = document.querySelectorAll('button')
